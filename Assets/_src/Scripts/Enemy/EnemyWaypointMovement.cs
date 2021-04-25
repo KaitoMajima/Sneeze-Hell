f40087686c29b40c99537f8f66fd592d7cfadac2
JetStream Sam <dorado.451@hotmail.com>
2021-04-25 19:06:33 -0300

    [POST JAM NOTICE] General balancing
    Implemented:
    - Now enemies can stop a period of seconds between their waypoints;
    
    Adjustments:
    - Ground detection radius has been slightly increased;
    - Fall toleration is now a little fairer;
    - The player's movement smooths less;
    - The player's bullet has a bigger light / affects the player.
    
    Balancing:
    - The Catsule's max health has been reduced to 1200 HP (from 1600 HP);
    - The Raging Hexagon's max health has been reduced to 3000 HP (from 5000 HP);
    - All slimes max health has been reduced to 400 HP (from 550 HP);
    - The triangle now waits 1 second between waypoints;
    - The boss big bullet damage has been reduced to 85 (from 100);
    - The boss bullet damage has been reduced to 30 (from 35);
    - The player's bullet damage has been reduced to 45 (from 50), as well as it's lifetime has been reduced to 0.8s (from 0.82s), and its movement maxspeed has been increased to 73 (from 65);
    - The player's sneeze damage has been increased to 130 (from 100), as well as its lifetime has been increased to 1s (from 0.9s);
     - The triangle's triangle bullet damage has been reduced to 50 (from 60);
    - The Catsule's long bullet damage has been reduced to 65 (from 70);

--- Assets/_src/Scripts/Enemy/EnemyWaypointMovement.cs
+++ Assets/_src/Scripts/Enemy/EnemyWaypointMovement.cs
@@ -11,14 +11,21 @@ namespace KaitoCo
     {
         [SerializeField] private Enemy enemy;
         [SerializeField] private Transform enemyTransform;
+        [SerializeField] private float pointInterval = 0;
         [SerializeField] private Transform[] movePoints;
         [SerializeField] private TweenSettings tweenSettings = TweenSettings.Default;
         private int currentWayPoint = 1;
+
         private void Start()
         {
             if(movePoints.Length == 0)
                 return;
-            enemyTransform.DOMove(movePoints[currentWayPoint].position, tweenSettings.duration).SetEase(tweenSettings.easeType).OnComplete(ChangeWayPoint);
+            
+            enemyTransform.DOMove(movePoints[currentWayPoint].position, tweenSettings.duration).SetEase(tweenSettings.easeType).
+            OnComplete(() => StartCoroutine(Wait(pointInterval, ChangeWayPoint)));
+
+            if(pointInterval <= 0)
+                return;
         }
 
         private void ChangeWayPoint()
@@ -26,7 +33,18 @@ namespace KaitoCo
             currentWayPoint++;
             if(currentWayPoint >= movePoints.Length)
                 currentWayPoint = 0;
-            enemyTransform.DOMove(movePoints[currentWayPoint].position, tweenSettings.duration).SetEase(tweenSettings.easeType).OnComplete(ChangeWayPoint);
+            enemyTransform.DOMove(movePoints[currentWayPoint].position, tweenSettings.duration).SetEase(tweenSettings.easeType).
+            OnComplete(() => StartCoroutine(Wait(pointInterval, ChangeWayPoint)));
+        }
+
+        private IEnumerator Wait(float seconds, Action callback)
+        {
+            while (seconds > 0)
+            {
+                seconds -= Time.deltaTime;
+                yield return null;
+            }
+            callback?.Invoke();
         }
     }
 
